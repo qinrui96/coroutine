7ea87698dd776992128a57ee56bfd81b7579dbd6
qr <qr@ndsl>
2021-08-10 17:52:36 +0800

    云风协程库

---
+++ build/CMakeFiles/coroutine.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/qr/c++/coroutine/coroutine.cc" "CMakeFiles/coroutine.dir/coroutine.cc.o" "gcc" "CMakeFiles/coroutine.dir/coroutine.cc.o.d"
+  "/home/qr/c++/coroutine/main.cc" "CMakeFiles/coroutine.dir/main.cc.o" "gcc" "CMakeFiles/coroutine.dir/main.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/coroutine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coroutine.dir/coroutine.cc.o"
+  "CMakeFiles/coroutine.dir/coroutine.cc.o.d"
+  "CMakeFiles/coroutine.dir/main.cc.o"
+  "CMakeFiles/coroutine.dir/main.cc.o.d"
+  "coroutine"
+  "coroutine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/coroutine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
